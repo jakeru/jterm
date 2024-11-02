04ec6bb5b78e0d0d8d3b764f5c106dbad5f480de
Jakob Ruhe <jakob.ruhe@gmail.com>
2024-11-02 23:48:01 +0100

    Continued work

--- jterm.py
+++ jterm.py
@@ -7,11 +7,16 @@ import serial
 import select
 import io
 import os
+import time
 import datetime
+import pathlib
+import re
+import xmodem
 
 from py_linenoise import linenoise
 
-HISTORY_FILE = os.path.join(os.environ.get("HOME", "/home"), ".jterm", "history.txt")
+APP_DATA_DIR = os.path.join(pathlib.Path.home(), ".jterm")
+HISTORY_FILE = os.path.join(APP_DATA_DIR, "history.txt")
 
 
 class LineBuf:
@@ -46,12 +51,14 @@ class Interface:
     def fileno(self):
         raise NotImplementedError()
 
+    def set_timeout(self, timeout_s):
+        raise NotImplementedError()
+
 
 class SerialInterface(Interface):
     def __init__(self, port, baudrate):
         self._port = port
         self._baudrate = baudrate
-        self.open()
 
     def read(self, size):
         return self.dev.read(size)
@@ -65,12 +72,17 @@ class SerialInterface(Interface):
     def open(self):
         self.dev = serial.Serial(port=self._port, baudrate=self._baudrate, timeout=0)
 
+    def set_timeout(self, timeout_s):
+        self.dev.timeout = timeout_s
+
+    def __str__(self):
+        return f"{self._port}"
+
 
 class SocketInterface(Interface):
     def __init__(self, host, port):
-        self.dev = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.dev.connect((host, port))
-        self.dev.settimeout(0)
+        self._host = host
+        self._port = port
 
     def read(self, size):
         try:
@@ -85,6 +97,17 @@ class SocketInterface(Interface):
 
     def fileno(self):
         return self.dev.fileno()
+
+    def open(self):
+        self.dev = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.dev.connect((self._host, self._port))
+        self.dev.settimeout(0)
+
+    def set_timeout(self, timeout_s):
+        self.dev.settimeout(timeout_s)
+
+    def __str__(self):
+        return f"{self._host}:{self._port}"
 
 
 def completion(s):
@@ -102,20 +125,80 @@ def hints(s):
     return None
 
 
-def process_cmd(cmdline, interface):
-    interface.write(cmdline.encode() + b"\r\n")
+def process_cmd(cmdline, interface, log_file, args):
+    eol = "\n"
+    if args.eof == "cr":
+        eol = b"\r"
+    elif args.eof == "crlf":
+        eol = b"\r\n"
+    elif args.eof == "lf":
+        pass
+    else:
+        raise ValueError(f"Bad argument for --eof '{args.eof}'")
+
+    if log_file is not None:
+        log_file.write(cmdline + "\n")
+
+    if args.delay_between_byte_ms > 0:
+        for c in cmdline.encode:
+            interface.write(c)
+            time.sleep(args.delay_between_byte_ms / 1000)
+    else:
+        interface.write(cmdline.encode())
+    time.sleep(args.delay_before_eol_ms / 1000)
+    interface.write(eol)
 
 
-def print_line(line):
+def remove_ansi_escape_codes(s):
+    # From:
+    # https://stackoverflow.com/questions/14693701/how-can-i-remove-the-ansi-escape-sequences-from-a-string-in-python
+    # 7-bit C1 ANSI sequences
+    ansi_escape = re.compile(
+        r"""
+        \x1B  # ESC
+        (?:   # 7-bit C1 Fe (except CSI)
+            [@-Z\\-_]
+        |     # or [ for CSI, followed by a control sequence
+            \[
+            [0-?]*  # Parameter bytes
+            [ -/]*  # Intermediate bytes
+            [@-~]   # Final byte
+        )
+    """,
+        re.VERBOSE,
+    )
+    return ansi_escape.sub("", s)
+
+
+def replace_non_printable(s, accept=""):
+    s_out = []
+    for c in s:
+        if c in accept:
+            s_out.append(c)
+        elif c == "\\":
+            s_out.append("\\\\")
+        elif c.isprintable():
+            s_out.append(c)
+        else:
+            s_out.append(f"\\x{ord(c):02x}")
+    return "".join(s_out)
+
+
+def print_line(line, log_file):
     time = datetime.datetime.now().isoformat(timespec="milliseconds")
     s = line.decode(errors="replace")
-    # Remove one trailing '\r' if it is present.
-    if s and s[-1] == "\r":
-        s = s[:-1]
-    print(f"{time} {s}")
+    s = s.replace("\r", "")
+    ESC = "\x1b"
+    s_print = replace_non_printable(s, ESC)
+    if ESC in s:
+        s_print = s_print + ESC + "[0m"
+    print(f"{time} {s_print}")
+    if log_file is not None:
+        s_log = replace_non_printable(remove_ansi_escape_codes(s))
+        log_file.write(f"{time} {s_log}\n")
 
 
-def process_input(ln, line_state, interface, prompt):
+def process_input(ln, line_state, interface, log_file, prompt, args):
     res = ln.edit_feed(line_state)
     if res == linenoise.EditResult.MORE:
         pass
@@ -128,29 +211,44 @@ def process_input(ln, line_state, interface, prompt):
         ln.edit_stop(line_state)
         cmdline = str(line_state)
         ln.history_add(cmdline)
-        process_cmd(cmdline, interface)
+        process_cmd(cmdline, interface, log_file, args)
         line_state = ln.edit_start(prompt)
     elif res == linenoise.EditResult.HOTKEY:
         ln.edit_stop(line_state)
         cmd = str(line_state)
         print("Hotkey detected, command line: '%s'" % cmd)
+        interface.write(linenoise._KEY_CTRL_L.encode())
         line_state = ln.edit_start(prompt, cmd)
     else:
         raise ValueError(res)
     return line_state
 
 
-def process_interface(line_state, line_buf, interface):
+def process_interface(line_state, line_buf, interface, log_file):
     while data := interface.read(1024):
         line_buf.write(data)
     if line_buf.has_line():
         line_state.hide()
         while (line := line_buf.readline()) is not None:
-            print_line(line)
+            print_line(line, log_file)
         line_state.show()
 
 
-def interactive(interface):
+def send_file(interface, filename):
+    def getc(size, timeout=1):
+        interface.set_timeout(timeout)
+        return interface.read(size)
+
+    def putc(data, _timeout=0):
+        interface.write(data)
+
+    with open(filename, "rb") as f:
+        print(f"Sending file '{filename}' to '{interface} using XMODEM protocol")
+        modem = xmodem.XMODEM(getc, putc)
+        modem.send(f, retry=16, timeout=10, quiet=False, callback=None)
+
+
+def interactive(interface, log_file, args):
     ln = linenoise.linenoise()
     line_buf = LineBuf()
 
@@ -163,6 +261,8 @@ def interactive(interface):
     # where entries are separated by newlines.
     ln.history_load(HISTORY_FILE)
 
+    ln.set_hotkey(linenoise._KEY_CTRL_L)
+
     prompt = "> "
     line_state = ln.edit_start(prompt)
     try:
@@ -171,10 +271,12 @@ def interactive(interface):
             (rd, _, _) = select.select(fds, (), ())
             if line_state.ifd in rd:
                 # Data is available on stdin (or EOF).
-                line_state = process_input(ln, line_state, interface, prompt)
+                line_state = process_input(
+                    ln, line_state, interface, log_file, prompt, args
+                )
             if interface.fileno() in rd:
                 # Data is available at our interface (or EOF).
-                process_interface(line_state, line_buf, interface)
+                process_interface(line_state, line_buf, interface, log_file)
     except EOFError:
         ln.edit_stop(line_state)
         os.makedirs(os.path.dirname(HISTORY_FILE), exist_ok=True)
@@ -215,6 +317,36 @@ def parse_args():
         default=1,
         help="Timeout in seconds",
     )
+    parser.add_argument(
+        "--delay_before_eol_ms",
+        type=int,
+        default=100,
+        help="Delay between command and end of line sequence (ms)",
+    )
+    parser.add_argument(
+        "delay_between_byte_ms",
+        type=int,
+        default=0,
+        help="Delay between each byte sent (ms)",
+    )
+    default_log_file_name = (
+        datetime.datetime.now().isoformat(sep="_", timespec="seconds").replace(":", "")
+        + ".log"
+    )
+    parser.add_argument(
+        "--log",
+        help="Log file to append to",
+        default=os.path.join(APP_DATA_DIR, "logs/" + default_log_file_name),
+    )
+    parser.add_argument(
+        "--sendfile",
+        help="Send a file by using the XMODEM protocol",
+    )
+    parser.add_argument(
+        "--eol",
+        default="lf",
+        help="End of line character(s), options: lf (default), crlf, cr",
+    )
     return parser.parse_args()
 
 
@@ -240,7 +372,34 @@ def main():
     else:
         print("Please specify either --socket or --serial.")
         sys.exit(1)
-    interactive(interface)
+
+    if args.eof != "crlf" and args.eof != "cr" and args.eof != "lf":
+        print("Please choose --eof to be crlf, cr or lf")
+        sys.exit(1)
+
+    first_try = True
+    while True:
+        try:
+            interface.open()
+            break
+        except serial.SerialException as e:
+            if first_try:
+                print(
+                    f"Failed to open {interface}: {e}. Will retry until stopped with ctrl+c."
+                )
+                first_try = False
+            time.sleep(0.1)
+
+    if args.sendfile:
+        send_file(interface, args.sendfile)
+        return
+
+    log_file = None
+    if args.log:
+        print(f"Appending to log file: '{args.log}'")
+        os.makedirs(os.path.dirname(args.log), exist_ok=True)
+        log_file = open(args.log, "a")
+    interactive(interface, log_file, args)
 
 
 if __name__ == "__main__":
